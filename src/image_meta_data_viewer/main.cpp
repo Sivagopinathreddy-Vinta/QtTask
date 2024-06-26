fb63bd972c52ed0ef3c8d92710a3df15ec30a543
Vinta Siva Gopinath Reddy <Siva-Gopinath-Reddy.Vinta@elektrobit.com>
2024-06-26 12:29:28 +0200

    use 5/3/0 rules

--- src/image_meta_data_viewer/main.cpp
+++ src/image_meta_data_viewer/main.cpp
@@ -2,6 +2,7 @@
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 #include "ImageMetaDataViewer.h"
+#include <memory>  
 
 int main(int argc, char *argv[]) {
     QGuiApplication app(argc, argv);
@@ -9,9 +10,11 @@ int main(int argc, char *argv[]) {
     qmlRegisterType<ImageMetaDataViewer>("com.ImageProcessor", 1, 0, "ImageProcessor");
 
     QQmlApplicationEngine engine;
-    ImageMetaDataViewer *processor = new ImageMetaDataViewer();
-    engine.rootContext()->setContextProperty("imageProcessor", processor);
-    QObject::connect(processor, &ImageMetaDataViewer::imageClicked, processor, &ImageMetaDataViewer::onImageClicked);
+    std::unique_ptr<ImageMetaDataViewer> processor = std::make_unique<ImageMetaDataViewer>();
+    engine.rootContext()->setContextProperty("imageProcessor", processor.get());
+
+    QObject::connect(processor.get(), &ImageMetaDataViewer::imageClicked,
+                     processor.get(), &ImageMetaDataViewer::onImageClicked); 
 
     const QUrl url(QStringLiteral("qrc:/main.qml"));
     engine.load(url);
